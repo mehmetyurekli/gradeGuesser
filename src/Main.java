a16d78c65bc33ebaae0325b2f97a0536ac2bfdcc
mehmetyurekli <yureklimehmet06@gmail.com>
2023-11-16 21:01:22 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        //I stored the data's in 3 different arrays because it seemed easier
         double[] x1 = {7.6, 8, 6.6, 8.4, 8.8, 7.2, 8.1, 9.5, 7.3, 8.9, 7.5, 7.6, 7.9, 8, 7.2, 8.8, 7.6, 7.5, 9, 7.7, 8.1};
         double[] x2 = {11, 10, 8, 10, 12, 10, 11, 9, 9, 11, 11, 9, 10, 10, 9, 10, 11, 10, 10, 9, 11};
         double[] target = {77, 70, 55, 78, 95, 67, 80, 87, 60, 88, 72, 58, 70, 76, 58, 81, 74, 67, 82, 62, 82};
 
+        //creating 3 different neurons and training them using different lambdas and epochs.
         Neuron neuronOne = new Neuron();
         neuronOne.load(x1, x2, target);
         neuronOne.setLambda(0.05);
-        neuronOne.train(10);
+        neuronOne.train(100);
 
         Neuron neuronTwo = new Neuron();
         neuronTwo.load(x1, x2, target);
-        neuronTwo.setLambda(0.01);
-        neuronTwo.train(50);
+        neuronTwo.setLambda(0.05);
+        neuronTwo.train(100);
 
         Neuron neuronThree = new Neuron();
         neuronThree.load(x1, x2, target);
-        neuronThree.setLambda(0.025);
+        neuronThree.setLambda(0.05);
         neuronThree.train(100);
 
-        System.out.println(neuronOne.getMse());
-        System.out.println(neuronTwo.getMse());
-        System.out.println(neuronThree.getMse());
+        //printing the results using the function .printGuesses. it also prints the mse by using .getMse
+        System.out.println("RESULTS OF NEURON - 1");
+        neuronOne.printGuesses();
 
-        System.out.printf("RESULTS OF NEURON - 1 | MSE = %,.5f\n", neuronOne.getMse());
-        neuronOne.printResults();
+        System.out.println("RESULTS OF NEURON - 2");
+        neuronTwo.printGuesses();
 
-        System.out.printf("RESULTS OF NEURON - 2 | MSE = %,.5f\n", neuronTwo.getMse());
-        neuronTwo.printResults();
+        System.out.println("RESULTS OF NEURON - 3");
+        neuronThree.printGuesses();
 
-        System.out.printf("RESULTS OF NEURON - 3 | MSE = %,.5f\n", neuronThree.getMse());
-        neuronThree.printResults();
-
-
-        System.out.println("GUESSES FROM UNTRAINED DATA:");
+        //I gave 5 custom inputs to the neuron to guess.
+        System.out.println("GUESSES FROM UNSEEN DATA:");
         System.out.printf("X1: %.1f | X2: %.1f -> Guess: %.0f\n", 10.1, 12.2, neuronTwo.guess(10, 12.2)*100);
         System.out.printf("X1: %.1f | X2: %.1f -> Guess: %.0f\n", 2.1, 3.5, neuronTwo.guess(2.1, 3.5)*100);
         System.out.printf("X1: %.1f | X2: %.1f -> Guess: %.0f\n", 6.5, 7.3, neuronTwo.guess(6.5, 7.3)*100);
--- src/Neuron.java
+++ src/Neuron.java
@@ -1,10 +1,7 @@
-
 import java.util.Random;
 
 public class Neuron {
-
-
-    private Random r = new Random();
+    private final Random r = new Random();
     public double w1;
     public double w2;
     private double[] x1s;
@@ -13,7 +10,7 @@ public class Neuron {
     private double lambda;
     private double mse;
 
-    public void load(double[] x1s, double[] x2s, double[] target){ //loading data to the neuron
+    public void load(double[] x1s, double[] x2s, double[] target) { //loading data to the neuron
         this.x1s = x1s;
         this.x2s = x2s;
         this.targetArray = target;
@@ -21,45 +18,50 @@ public class Neuron {
         this.w2 = r.nextDouble(0, 1);
     }
 
-    public double guess(double x1, double x2){ // makes guesses using the formula given
-        double actualGuess = ((x1/15*w1)+(x2/10*w2));
-        if(actualGuess>1){
+    public double guess(double x1, double x2) { // makes guesses using the formula given
+        double actualGuess = ((x1 / 15 * w1) + (x2 / 10 * w2));
+        // I actually added this part because if I give the neuron values like 50, 60; it was returning guesses
+        // over 100. so I set up the max and minimum grade.
+        if (actualGuess > 1) {
             return 1;
         }
-        if(actualGuess<0){
+        if (actualGuess < 0) {
             return 0;
         }
         return actualGuess;
     }
-    public void train(int epoch){ // trains the neuron from the loaded set (epoch x sample number) times.
-        double sumOfSquares = 0; // mse is the average of sum of squares.
-        int count = 0;
-        for(int i = 0; i<epoch; i++){
-            for(int j = 0; j < x1s.length; j++){
-                double target = targetArray[j]/100;
+
+    public void train(int epoch) { // trains the neuron from the loaded set (epoch x sample number) times.
+        for (int i = 0; i < epoch; i++) {
+            for (int j = 0; j < x1s.length; j++) {
+                double target = targetArray[j] / 100;
                 double guess = guess(x1s[j], x2s[j]);
-                this.w1 = w1 + lambda*(target-guess)*x1s[j];
-                this.w2 = w2 + lambda*(target-guess)*x2s[j];
-                sumOfSquares += Math.pow(target-guess, 2);
-                count++;
+                this.w1 = w1 + lambda * (target - guess) * x1s[j]/10;
+                this.w2 = w2 + lambda * (target - guess) * x2s[j]/15;
             }
         }
-        mse = sumOfSquares/count;
     }
-    public void printResults(){
+
+    public void printGuesses() { // makes guesses and prints the inputs, the output and the expected values
+        double sumOfSquares = 0; // mse is the average of sum of squares.
         System.out.printf("| %6s | %6s | %6s | %6s |\n", "X1", "X2", "TARGET", "GUESS");
         System.out.println("-------------------------------------");
-        for(int i=0; i<x1s.length; i++){
-            System.out.printf("| %6s | %6s | %6s | %6.2f |\n", x1s[i], x2s[i], targetArray[i], guess(x1s[i], x2s[i])*100);
+        for (int i = 0; i < x1s.length; i++) {
+            double target = targetArray[i] / 100;
+            double guess = guess(x1s[i], x2s[i]);
+            System.out.printf("| %6s | %6s | %6s | %6.2f |\n", x1s[i], x2s[i], targetArray[i], guess * 100);
+            sumOfSquares += Math.pow(target - guess, 2);
         }
+        this.mse = sumOfSquares/x1s.length;
+        System.out.printf("MSE= %,.5f", mse);
         System.out.println();
     }
 
-    public double getMse() {
+    public double getMse() { // mse getter
         return mse;
     }
 
-    public void setLambda(double lambda) {
+    public void setLambda(double lambda) { //lambda setter
         this.lambda = lambda;
     }
 }
